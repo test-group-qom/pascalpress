55d89e863a094c5d9059801ea5b8d8073fda321d
mary roshan <m.roshan50@yahoo.com>
2017-03-09 01:04:35 +0330

    improve api.php by grouping routes including middleware

--- routes/api.php
+++ routes/api.php
@@ -23,39 +23,33 @@ Route::get('news',  [ 'as' => 'news.index', 'uses' => 'newsController@index']);
 Route::get('news/{id}',  [ 'as' => 'news.show', 'uses' => 'newsController@show']);
 Route::get('search', [ 'as' => 'news.search', 'uses' => 'newsController@search']);
 
-//Route::get('news/create',  [ 'as' => 'news.create', 'uses' => 'newsController@create', 'middleware' =>myAuth::class]);
-Route::put('news/{id}',  [ 'as' => 'news.update', 'uses' => 'newsController@update', 'middleware' =>myAuth::class]);
-Route::post('news',  [ 'as' => 'news.create', 'uses' => 'newsController@store', 'middleware' =>myAuth::class]);
-//Route::delete('news/{id}',  [ 'as' => 'news.index', 'uses' => 'newsController@destroy', 'middleware' =>myAuth::class]);
-Route::delete('news/{id}',  [ 'as' => 'news.index', 'uses' => 'newsController@delete', 'middleware' =>myAuth::class]);
-Route::put('news/{id}/restore',  [ 'as' => 'news.index', 'uses' => 'newsController@restore', 'middleware' =>myAuth::class]);
+Route::group(['middleware'=>myAuth::class], function(){
+    Route::post('news',  [ 'as' => 'news.create', 'uses' => 'newsController@store']);
+	Route::put('news/{id}',  [ 'as' => 'news.create', 'uses' => 'newsController@update']);
+	//Route::delete('news/{id}',  [ 'as' => 'news.index', 'uses' => 'newsController@destroy', 'middleware' =>myAuth::class]);
+	Route::delete('news/{id}',  [ 'as' => 'news.index', 'uses' => 'newsController@destroy']);
+    Route::put('news/{id}',  [ 'as' => 'news.index', 'uses' => 'newsController@delete']);
+    Route::put('news/{id}/restore',  [ 'as' => 'news.index', 'uses' => 'newsController@restore']);
+});
 
 // article routes.......................................................................................
 Route::get('article',  [ 'as' => 'article.index', 'uses' => 'articleController@index']);
 Route::get('article/{id}',  [ 'as' => 'article.show', 'uses' => 'articleController@show']);
 Route::get('search', [ 'as' => 'article.search', 'uses' => 'articleController@search']);
 
-Route::put('article/{id}',  [ 'as' => 'article.update', 'uses' => 'articleController@update', 'middleware' =>myAuth::class]);
-Route::post('article',  [ 'as' => 'article.create', 'uses' => 'articleController@store', 'middleware' =>myAuth::class]);
-//Route::delete('news/{id}',  [ 'as' => 'news.index', 'uses' => 'newsController@destroy', 'middleware' =>myAuth::class]);
-Route::delete('article/{id}',  [ 'as' => 'article.index', 'uses' => 'articleController@delete', 'middleware' =>myAuth::class]);
-Route::put('article/{id}/restore',  [ 'as' => 'article.index', 'uses' => 'articleController@restore', 'middleware' =>myAuth::class]);
-
+Route::group(['middleware'=>myAuth::class], function(){
+	Route::put('article/{id}',  [ 'as' => 'article.update', 'uses' => 'articleController@update']);
+	Route::post('article',  [ 'as' => 'article.create', 'uses' => 'articleController@store']);
+	//Route::delete('news/{id}',  [ 'as' => 'news.index', 'uses' => 'newsController@destroy', 'middleware' =>myAuth::class]);
+	Route::delete('article/{id}',  [ 'as' => 'article.index', 'uses' => 'articleController@delete']);
+	Route::put('article/{id}/restore',  [ 'as' => 'article.index', 'uses' => 'articleController@restore']);
+});
 // login routes..........................................................................................
 Route::post('login',  [ 'as' => '', 'uses' => 'Auth\LoginController@login']);
 Route::get('logout',  [ 'as' => '', 'uses' => 'Auth\LoginController@logout', 'middleware' =>myAuth::class]);
 Route::post('register',  [ 'as' => '', 'uses' => 'Auth\RegisterController@create_api']);
 //.......................................................................................................
 
-
-// Route::group(['prefix'=>'api','middleware'=>myAuth::class], function(){
-// 	Route::get('news',  [ 'as' => 'news.index', 'uses' => 'newsController@index']);
-//     Route::post('news',  [ 'as' => 'news.create', 'uses' => 'newsController@store']);
-// 	Route::put('news/{id}',  [ 'as' => 'news.create', 'uses' => 'newsController@update']);
-// 	Route::delete('news/{id}',  [ 'as' => 'news.index', 'uses' => 'newsController@destroy']);
-//     Route::put('news/{id}',  [ 'as' => 'news.index', 'uses' => 'newsController@delete']);
-//     Route::put('news/{id}/restore',  [ 'as' => 'news.index', 'uses' => 'newsController@restore']);
-// });
 
 Route::get('user',  [ 'as' => 'user.index', 'uses' => 'userController@index']);
 Route::get('user/{id}',  [ 'as' => 'user.show', 'uses' => 'userController@show']);
