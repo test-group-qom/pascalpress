7a263ff36b20e3f8498ccb50611e67dc24095190
RM <iransoft1390@gmail.com>
2017-10-12 15:19:21 +0330

    cache fix bug.

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -2,7 +2,6 @@
 <html lang="{{ app()->getLocale() }}">
   <head>
     <meta charset="utf-8">
-    <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="{{$config->description}}">
